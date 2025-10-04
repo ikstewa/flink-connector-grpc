0ae035d550a3cda4bade87547c8690282fb4703f
Ian Stewart <istewart@kollective.com>
2025-10-03 22:06:22 -0700

    Setup gradle formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,31 @@ import net.researchgate.release.ReleaseExtension
 plugins {
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("net.researchgate.release") version "3.1.0"
+    id("com.diffplug.spotless") version "8.0.0"
 }
 
-configure<ReleaseExtension> {
-    with(git) {
-        requireBranch.set("master")
+configure<ReleaseExtension> { with(git) { requireBranch.set("master") } }
+
+allprojects {
+    apply(plugin = "com.diffplug.spotless")
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    spotless {
+        format("misc") {
+            target("*.md", ".gitignore")
+
+            trimTrailingWhitespace()
+            leadingTabsToSpaces()
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint()
+        }
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,9 @@
 //     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 // }
 rootProject.name = "flink-connector-grpc"
+
 include("flink-connector-grpc")
+
 include("example")
+
 include("mock-rpc-server")
