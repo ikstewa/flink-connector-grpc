83cf82f609d79ddbaf13adad0bc78a9a9bf09b4c
Ian Stewart <ikstewa@gmail.com>
2025-11-22 11:01:03 -0800

    Upgrade gradle wrapper (#163)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,8 @@
-// plugins {
-//     // Apply the foojay-resolver plugin to allow automatic download of JDKs
-//     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-// }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 rootProject.name = "flink-connector-grpc"
 
 include("flink-connector-grpc")
-
 include("example")
-
 include("mock-rpc-server")
